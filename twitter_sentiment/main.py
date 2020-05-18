7c347a72d903492fba013eff9e333cb68a646443
Catherine <catherinegitau94@gmail.com>
2020-05-18 17:14:57 +0000

    commented out twitter scraper import

--- twitter_sentiment/main.py
+++ twitter_sentiment/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 from flair.data import Sentence
 from flair.models import TextClassifier
-from twitterscraper import query_tweets
+#from twitterscraper import query_tweets
 
 
 # Set page title
